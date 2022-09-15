6350c1e5137a4c952abf009ea5077745a6717c1b
Bipin Lekhak <bplthegreat@gmail.com>
2022-09-15 12:33:51 -0400

    Label plots

--- Project 1/model_selection.m
+++ Project 1/model_selection.m
@@ -10,7 +10,6 @@ best_p = zeros(max_p, 3);
 
 for p = 0:max_p
     z = basis_func(X, p);
-
     [test_error, train_error] = KFoldCrossVal(z, Y, K_val);
     best_p(p+1,:) = [p, train_error, test_error];
 end
@@ -20,6 +19,10 @@ plot(best_p(:,1), best_p(:,2));
 hold on;
 plot(best_p(:,1), best_p(:,3));
 ylim([0, max(best_p(:,2))])
+xlabel("max\_p")
+ylabel("error")
+legend("train\_error", "test\_error")
+title("Model selection using K-fold cross validation")
 
 [~, arg_min_test] = min(best_p(:,3));
 
