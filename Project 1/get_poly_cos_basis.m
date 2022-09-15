49418552ef81082c9cb656eac583465a16629161
Bipin Lekhak <bplthegreat@gmail.com>
2022-09-14 21:13:53 -0400

    Modified to use cos

--- Project 1/get_poly_cos_basis.m
+++ Project 1/get_poly_cos_basis.m
@@ -1,7 +1,7 @@
 function z = get_poly_cos_basis(X, p)
     z_1 = get_poly_basis(X, p);
     z_2 = 5 * cos(X);
-    z_4 = log(1 - X);
-    z = [z_1; z_2; z_4];
+%     z_4 = log(1 - X);
+    z = [z_1; z_2];
 
 end
--- Project 1/project_run.m
+++ Project 1/project_run.m
@@ -14,15 +14,17 @@ Xtest = importdata('data/testinputs.txt');
 X = norm_min_max(X);
 Xtest = norm_min_max(Xtest);
 
-[p_best, train_error_best, test_error_best] = model_selection(X', Y, @get_poly_basis);
+[p_best, train_error_best, test_error_best] = model_selection(X', Y, @get_poly_cos_basis);
 
 z = get_poly_basis(X', p_best);
 ztest = get_poly_basis(Xtest', p_best);
 [w_vec, t_error] = LeastSquareBasis(z, Y');
 
-Ytest = w_vec' * ztest;
+Ypred = (w_vec' * z)';
+Ytest = (w_vec' * ztest)';
 
-Ytest = Ytest';
+train_error = norm(Ypred - Y) ^ 2 / length(Y)
+pred_test_error = test_error_best
 
 save("predictions.txt", 'Ytest', '-ascii')
 
