8104b3d271d2f565617785e178f74fa3bb047d23
Bipin Lekhak <bplthegreat@gmail.com>
2022-11-02 17:13:03 -0400

    Change to dist using samples from [0,1]

--- Project 3/check_ci.m
+++ Project 3/check_ci.m
@@ -1,20 +1,26 @@
 function [percent_missing] = check_ci(N, func_num, check_num)
 
-if check_num == 1
+if check_num == 1 % "Beta(2,2)"
     act_mu = 0.5;
+    sampler = @(x) betarnd(2,2, x, 1);
+elseif check_num == 2 % "Beta(20000,1)"
+    act_mu = 20000 / (20000 + 1);
+    sampler = @(x) betarnd(20000, 1, x, 1);
+elseif check_num == 3 % "Beta(1,20000)"
+    act_mu = 1 / (20000 + 1);
+    sampler = @(x) betarnd(1, 20000, x, 1);
+elseif check_num == 4 % "Bern(0.99999)"
+    act_mu = 0.99999;
     sampler = @(x) sample_bernoulli(x, act_mu);
-elseif check_num == 2
-    act_mu = 20;
-    sampler = @(x) sample_uniform(x, act_mu - 10, act_mu + 10);
-elseif check_num == 3
-    act_mu = -95;
-    sampler = @(x) sample_uniform(x, act_mu - 5, act_mu + 5);
-elseif check_num == 4
-    act_mu = -0.5;
-    sampler = @(x) sample_normal(x, 4, act_mu);
-elseif check_num == 5
-    act_mu = 10;
-    sampler = @(x) (sample_normal(x, 1, act_mu-10) + sample_normal(x, 1, act_mu+10))/2;
+elseif check_num == 5 % "Bern(0.00001)"
+    act_mu = 0.00001;
+    sampler = @(x) sample_bernoulli(x, act_mu);
+elseif check_num == 6 % "U(0, 0.001)"
+    act_mu = (0.001 + 0)/2;
+    sampler = @(x) sample_uniform(x, 0, 0.001);
+elseif check_num == 7 % "U(0.999, 1)"
+    act_mu = (0.999 + 1)/2;
+    sampler = @(x) sample_uniform(x, 0.999, 1);
 end
 
 precent_mu = 0;
