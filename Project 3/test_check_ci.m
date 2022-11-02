d94b8b5ef9bb69cc528060a5a820a9125c6d51e6
Bipin Lekhak <bplthegreat@gmail.com>
2022-11-02 09:30:41 -0400

    File outputs

--- Project 3/test_check_ci.m
+++ Project 3/test_check_ci.m
@@ -1,4 +1,13 @@
-function test_check_ci(func_num)
+function test_check_ci(func_num, varargin)
+
+if nargin == 2
+    fname = varargin{1}
+    fid = fopen(fname + "_" + string(func_num) +".txt", "a")
+else
+    fid = 1;
+end
+
+
 
 sampler_names = [...
     "Ber(0.5)", ...
@@ -10,15 +19,20 @@ sampler_names = [...
 
 for sampler_num = 1:5
     name = sampler_names(sampler_num);
-    fprintf("%s\n", [name]);
+    fprintf(fid,"%s\n", name);
     for N = 10:50:1011
         missing = check_ci(N, func_num, sampler_num);
-        fprintf( ...
+        fprintf( fid,...
             "N =%d \t missing=%.4f \n", ...
             [N, missing] ...
-        )
+        );
     end
 end
+
+if nargin == 2
+    fclose(fid);
+end
+
 end
 
 
